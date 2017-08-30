a1feb69087fc733b687b813c5b34c2d775fdb708
Juliusgeo <juliusgeo@gmail.com>
2017-08-30 04:37:32 -0400

    added saturation filter

--- filters.py
+++ filters.py
@@ -8,12 +8,20 @@ w, h = im.size
 imarray = np.asarray(im).copy()
 #print(imarray[i - 1:i + 1, n - 1:n + 1].reshape(1,4,3)[0][:])
 def medianDeNoise(imarray,w,h):
-    for i in range(1,h-2):
-        for n in range(1,w-2):
-            #neighborsmatrix=np.asarray([imarray[i - 1::i + 1, n - 1].reshape(1,2,3)[0][:],imarray[i - 1:i + 1, n].reshape(1,2,3)[0][:],imarray[i - 1:i + 1,n + 1].reshape(1,2,3)[0][:][:]]).reshape(1,6,3)
-            #print(neighborsmatrix[0][:][:])
+    for i in range(1,h-1):
+        for n in range(1,w-1):
             neighborsmatrix=np.asarray([imarray[i-1,n-1,:],imarray[i+1,n-1,:],imarray[i,n-1,:],imarray[i+1,n,:],imarray[i-1,n,:],imarray[i-1,n+1,:],imarray[i,n+1,:],imarray[i+1,n+1,:],]).reshape(1,8,3)
             imarray[i,n]=np.median(neighborsmatrix[0][:][:], axis=0)
     return imarray
-image = Image.fromarray(medianDeNoise(imarray,w,h), 'RGB')
+def saturationIncrease(imarray, amt, w, h):
+    for i in range(1,h-1):
+        for n in range(1,w-1):
+            hichannel=np.argmax(imarray[i,n])
+            if(imarray[i,n][hichannel]+amt<255):
+                imarray[i,n][hichannel]+=amt
+            else:
+                imarray[i,n][hichannel]=255
+    return imarray
+#image = Image.fromarray(medianDeNoise(imarray,w,h), 'RGB')
+image = Image.fromarray(saturationIncrease(imarray,40,w,h), 'RGB')
 image.show()
